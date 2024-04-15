3cf51c128851b925c9d64076e8f7cd32a88798e9
Alfredo Gutierrez <82833619+notalfredo@users.noreply.github.com>
2024-04-14 19:53:28 -0500

    modulo operator (#61)

--- tests/semantic_test.cpp
+++ tests/semantic_test.cpp
@@ -305,3 +305,39 @@ TEST(semantic, testEight) {
     fclose(filePtr);
 }
 
+
+TEST(semantic, testNine) {
+    
+    FILE* filePtr = fopen("/home/alfredo/repos/OpenKittenCad/tests/input_tests/semanticInputNine.kts", "r");
+    
+    if(!filePtr){
+        fprintf(stderr, "Error opening file semantic testOne\n");
+        fclose(filePtr); 
+    }
+
+     
+    //yydebug = 1;
+    yyscan_t scanner;
+    yylex_init(&scanner);
+    yyrestart(filePtr, scanner);
+    yyset_lineno(1, scanner);
+
+    void *result = NULL;
+    int parseState = yyparse(scanner, &result);
+
+    
+    if(parseState != 0){
+        fprintf(stderr, "Error parsing exiting...\n");
+        CHECK(0);
+    }
+    else {
+        fprintf(stdout, "Now performing semantic analysis\n\n");        
+        semantic((NodeStmtList*)result, 0);
+        CHECK(1);
+    }
+
+
+    yylex_destroy(scanner);
+    freeAllNodes();
+    fclose(filePtr);
+}
