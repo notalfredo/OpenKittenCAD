08192086b9ea6770e5fa9e47a95b88004f3119a8
Alfredo <icealfredo123@gmail.com>
2024-04-21 16:55:33 -0500

    small clean up

--- src/back-end/functions.hxx
+++ src/back-end/functions.hxx
@@ -3,42 +3,78 @@
 #include "node.hxx"
 
 
+enum twoDim {
+    makePoint,
+    makeEdge,
+    makeArc,
+    makeFace,
+};
 
-enum functionEnum {
+
+enum threeDim {
     makeSphere,
     makeCone,
     makeCylinder,
     makeBox,
-    makeUnion,
-    makeDifference,
-    makeIntersection,
     makeTorus,
-    makeFace,
+};
 
-    lineTo,
-    
-    doRotate,
-    doTranslate,
-    doMirror,
-    doExtrude,
-    doFillet,
-    doChamfer,
 
-    makePoint,
-    makeEdge,
-    makeArc,
-    connect,
-    revol,   
+enum booleanOperations {
+    performUnion,
+    performDifference,
+    performIntersection,
+};
 
-    printDouble,
-    addShape
+
+enum transformation {
+    performRotate,
+    performTranslate,
+    performMirror,
+};
+
+
+enum geomOp {
+    performExtrude,
+    performFillet,
+    performChamfer,
+    performRevolve,
+};
+
+
+enum miscFunc {
+    print,
+    addShape,
+    createConnection,
+    createLineTo,
+};
+
+
+union functionCategory {
+    twoDim twoDimCall;
+    threeDim threeDimCall;
+    booleanOperations booleanOperationsCall;
+    transformation transformationCall;
+    geomOp geomOpCall;
+    miscFunc miscFuncCall;
+};
+
+
+enum functionType {
+    twoDim,
+    threeDim,
+    booleanOperations,
+    transformation,
+    miscFunc,
+    geomOp,
 };
 
 
 
 typedef struct functionPtr{
     const char* name;
-    functionEnum functionType;
+    functionType funcType;
+    functionCategory functionVal;
 }functionPtr;
 
 
